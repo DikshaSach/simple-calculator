af34ca0e8ff4ee5a788c6b5c2f3e971b3aa91192
dixiexxx <sachdiksha@gmail.com>
2016-01-08 23:07:21 -0500

    inital commit

---
+++ calcscript.js
@@ -0,0 +1,54 @@
+function run1(){
+ document.frm.result.value +="1";
+}
+function run2(){
+ document.frm.result.value +="2";
+}
+function run3(){
+ document.frm.result.value +="3";
+}
+function run4(){
+ document.frm.result.value +="4";
+}
+function run5(){
+ document.frm.result.value +="5";
+}
+function run6(){
+ document.frm.result.value +="6";
+}
+function run7(){
+ document.frm.result.value +="7";
+}
+function run8(){
+ document.frm.result.value +="8";
+}
+function run9(){
+ document.frm.result.value +="9";
+}
+function run0(){
+ document.frm.result.value +="0";
+}
+
+
+function runplus(){
+ document.frm.result.value +="+";
+}
+function runsub(){
+ document.frm.result.value +="-";
+}
+function runmul(){
+ document.frm.result.value +="*";
+}
+function rundiv(){
+ document.frm.result.value +="/";
+}
+function evalu(){
+  var evalo=eval(document.frm.result.value)
+  document.frm.result.value=evalo;
+}
+function percent(){
+  document.frm.result.value +=".";
+}
+function runclear(){
+  document.frm.result.value ="";
+}
